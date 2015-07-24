3dfcdc486db720f4fe92224f1175211802e7a7f3
morishitter <morishitter@gmail.com>
2015-07-24 19:22:08 +0900

    write test code

--- test/index.js
+++ test/index.js
@@ -1,6 +1,44 @@
 var test = require('tape')
-var is-color = require('..')
+var color = require('..')
 
-test('description', function (t) {
-    t.end()
+test('isRgb', function (t) {
+  var str = 'rgb(255, 0, 0)'
+  t.same(color.isRgb(str), true)
+  t.end()
+})
+
+test('isRgba', function (t) {
+  var str = 'rgba(255, 0, 0, .5)'
+  t.same(color.isRgba(str), true)
+  t.end()
+})
+
+test('isHsl', function (t) {
+  var str = 'hsl(123, 45%, 67%)'
+  t.same(color.isHsl(str), true)
+  t.end()
+})
+
+test('isHsla', function (t) {
+  var str = 'hsla(123, 45%, 67%, 0.9)'
+  t.same(color.isHsla(str), true)
+  t.end()
+})
+
+test('isHex', function (t) {
+  var str = '#123456'
+  t.same(color.isHex(str), true)
+  t.end()
+})
+
+test('isKeyword', function (t) {
+  var str = 'tomato'
+  t.same(color.isKeyword(str), true)
+  t.end()
+})
+
+test('isColor', function (t) {
+  var str = 'tomato'
+  t.equal(color(str), true)
+  t.end()
 })
