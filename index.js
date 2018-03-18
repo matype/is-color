1b58ba17865e0d05b597c533283db643a71a2fec
Masaaki Morishita <morishitter@gmail.com>
2018-03-18 19:30:47 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var isRgb = require('./lib/isRgb')
-var isRgba = require('./lib/isRgba')
-var isHsl = require('./lib/isHsl')
-var isHsla = require('./lib/isHsla')
-var isHex = require('./lib/isHex')
-var isKeyword = require('./lib/isKeyword')
-var isInherit = require('./lib/isInherit')
-var isCurrentColor = require('./lib/isCurrentColor')
-var isTransparent = require('./lib/isTransparent')
+const rgbRegex = require('rgb-regex')
+const rgbaRegex = require('rgba-regex')
+const hslRegex = require('hsl-regex')
+const hslaRegex = require('hsla-regex')
+const hexRegex = require('hex-color-regex')
+const keywords = require('css-color-names')
 
+const isRgb = str => rgbRegex({exact: true}).test(str)
+const isRgba = str => rgbaRegex({exact: true}).test(str)
+const isHsl = str => hslRegex({exact: true}).test(str)
+const isHsla = str => hslaRegex({exact: true}).test(str)
+const isHex = str => hexRegex({exact: true}).test(str)
+const isKeyword = str => keywords[str] ? true : false
+const isInherit = str => str === 'inherit'
+const isCurrentColor = str => str === 'currentColor' || str === 'currentcolor'
+const isTransparent = str => str === 'transparent'
+const isColor = str => isRgb(str) || isRgba(str) || isHsl(str) || isHsla(str) || isHex(str) || isKeyword(str) || isInherit(str) || isCurrentColor(str) || isTransparent(str)
 
-module.exports = function isColor (str) {
-  return isRgb(str) || isRgba(str) || isHsl(str) || isHsla(str) || isHex(str) || isKeyword(str) || isInherit(str) || isCurrentColor(str) || isTransparent(str)
-}
-
+module.exports = isColor
 module.exports.isRgb = isRgb
 module.exports.isRgba = isRgba
 module.exports.isHsl = isHsl
--- test/index.js
+++ test/index.js
@@ -1,74 +1,74 @@
-var test = require('tape')
-var color = require('..')
+const test = require('tape')
+const color = require('..')
 
 test('isRgb', function (t) {
-  var str = 'rgb(255, 0, 0)'
+  const str = 'rgb(255, 0, 0)'
   t.same(color.isRgb(str), true)
   t.end()
 })
 
 test('isRgba', function (t) {
-  var str = 'rgba(255, 0, 0, .5)'
+  const str = 'rgba(255, 0, 0, .5)'
   t.same(color.isRgba(str), true)
   t.end()
 })
 
 test('isHsl', function (t) {
-  var str = 'hsl(123, 45%, 67%)'
+  const str = 'hsl(123, 45%, 67%)'
   t.same(color.isHsl(str), true)
   t.end()
 })
 
 test('isHsla', function (t) {
-  var str = 'hsla(123, 45%, 67%, 0.9)'
+  const str = 'hsla(123, 45%, 67%, 0.9)'
   t.same(color.isHsla(str), true)
   t.end()
 })
 
 test('isHex', function (t) {
-  var str = '#123456'
+  const str = '#123456'
   t.same(color.isHex(str), true)
   t.end()
 })
 
 test('isHex with alpha channel', function (t) {
-  var str = '#0000ffcc'
+  const str = '#0000ffcc'
   t.same(color.isHex(str), true)
   t.end()
 })
 
 test('isKeyword', function (t) {
-  var str = 'tomato'
+  const str = 'tomato'
   t.same(color.isKeyword(str), true)
   t.end()
 })
 
 test('isColor', function (t) {
-  var str = 'tomato'
+  const str = 'tomato'
   t.equal(color(str), true)
   t.end()
 })
 
 test('isColor', function (t) {
-  var str = 'currentColor'
+  const str = 'currentColor'
   t.equal(color(str), true)
   t.end()
 })
 
 test('isColor', function (t) {
-  var str = 'currentColor'
+  const str = 'currentColor'
   t.equal(color(str), true)
   t.end()
 })
 
 test('isColor', function (t) {
-  var str = 'inherit'
+  const str = 'inherit'
   t.equal(color(str), true)
   t.end()
 })
 
 test('isColor', function (t) {
-  var str = 'transparent'
+  const str = 'transparent'
   t.equal(color(str), true)
   t.end()
 })
