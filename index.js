5fa454b51723c76f9fb5964bf971539cf9b0a76b
Levi Fuller <lfuller941@gmail.com>
2018-04-25 15:09:08 -0600

    Reverted to ES5 code to avoid uglification errors;

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const hslaRegex = require('hsla-regex')
 const hexRegex = require('hex-color-regex')
 const keywords = require('css-color-names')
 
-const isRgb = str => rgbRegex({exact: true}).test(str)
-const isRgba = str => rgbaRegex({exact: true}).test(str)
-const isHsl = str => hslRegex({exact: true}).test(str)
-const isHsla = str => hslaRegex({exact: true}).test(str)
-const isHex = str => hexRegex({exact: true}).test(str)
-const isKeyword = str => keywords[str] ? true : false
-const isInherit = str => str === 'inherit'
-const isCurrentColor = str => str === 'currentColor' || str === 'currentcolor'
-const isTransparent = str => str === 'transparent'
-const isColor = str => isRgb(str) || isRgba(str) || isHsl(str) || isHsla(str) || isHex(str) || isKeyword(str) || isInherit(str) || isCurrentColor(str) || isTransparent(str)
+const isRgb = function (str) { return rgbRegex({exact: true}).test(str) }
+const isRgba = function (str) { return rgbaRegex({exact: true}).test(str) }
+const isHsl = function (str) { return hslRegex({exact: true}).test(str) }
+const isHsla = function (str) { return hslaRegex({exact: true}).test(str) }
+const isHex = function (str) { return hexRegex({exact: true}).test(str) }
+const isKeyword = function (str) { return keywords[str] ? true : false }
+const isInherit = function (str) { return str === 'inherit' }
+const isCurrentColor = function (str) { return str === 'currentColor' || str === 'currentcolor' }
+const isTransparent = function (str) { return str === 'transparent' }
+const isColor = function (str) { return isRgb(str) || isRgba(str) || isHsl(str) || isHsla(str) || isHex(str) || isKeyword(str) || isInherit(str) || isCurrentColor(str) || isTransparent(str) }
 
 module.exports = isColor
 module.exports.isRgb = isRgb
