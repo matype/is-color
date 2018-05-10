c632da3ba092d389231b551e2d8925ef25a7131e
hyurl <i@hyurl.com>
2018-05-10 17:31:47 +0800

    check rgb and rgba strictly.

--- index.js
+++ index.js
@@ -5,11 +5,34 @@ const hslaRegex = require('hsla-regex')
 const hexRegex = require('hex-color-regex')
 const keywords = require('css-color-names')
 
-const isRgb = function (str) { return rgbRegex({exact: true}).test(str) }
-const isRgba = function (str) { return rgbaRegex({exact: true}).test(str) }
-const isHsl = function (str) { return hslRegex({exact: true}).test(str) }
-const isHsla = function (str) { return hslaRegex({exact: true}).test(str) }
-const isHex = function (str) { return hexRegex({exact: true}).test(str) }
+const isValidRgb = function (matches) {
+    for (var i in matches) {
+        if (matches[i] < 0 || matches[i] > 255)
+            return false;
+    }
+
+    return true;
+}
+
+const isRgb = function (str) {
+    var matches = rgbRegex({ exact: true }).exec(str);
+
+    if (!matches) return false;
+
+    return isValidRgb(matches.slice(1));
+}
+
+const isRgba = function (str) {
+    var matches = rgbaRegex({ exact: true }).exec(str);
+
+    if (!matches || matches[4] < 0 || matches[4] > 1) return false;
+
+    return isValidRgb(matches.slice(1, 4));
+}
+
+const isHsl = function (str) { return hslRegex({ exact: true }).test(str) }
+const isHsla = function (str) { return hslaRegex({ exact: true }).test(str) }
+const isHex = function (str) { return hexRegex({ exact: true }).test(str) }
 const isKeyword = function (str) { return keywords[str] ? true : false }
 const isInherit = function (str) { return str === 'inherit' }
 const isCurrentColor = function (str) { return str === 'currentColor' || str === 'currentcolor' }
